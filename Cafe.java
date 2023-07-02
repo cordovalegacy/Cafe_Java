0454ddc54ea5809fb33b2923e98c0df4bb93331a
Brendan Cordova <cordovalegacy19@gmail.com>
2023-07-02 16:17:19 -0700

    Setup for cafe assignment

---
+++ Cafe.java
@@ -0,0 +1,18 @@
+import static java.lang.System.out;
+
+public static void main(String[] args) {
+
+    String greetingMessage = "Welcome to Cafe Java, ";
+    String pendingMessage = ", your order will be ready shortly";
+    String readyMessage = ", your order is ready";
+    String displayTotalMessage = "Your total is $";
+
+    double whiteMochaPrice = 5.5;
+
+    String customer1 = "Brendan Cordova";
+
+    boolean isreadyOrder1 = false;
+
+    out.println(greetingMessage + customer1)
+
+}
