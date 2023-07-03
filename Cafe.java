a634cafcf8474f059010ab71a51ab1e6f3221a41
Brendan Cordova <cordovalegacy19@gmail.com>
2023-07-02 19:38:08 -0700

    Done

--- Cafe.java
+++ Cafe.java
@@ -1,18 +1,49 @@
 import static java.lang.System.out;
 
-public static void main(String[] args) {
+public class Cafe{
 
-    String greetingMessage = "Welcome to Cafe Java, ";
-    String pendingMessage = ", your order will be ready shortly";
-    String readyMessage = ", your order is ready";
-    String displayTotalMessage = "Your total is $";
+    public static String orderUp(String name, double price, boolean ready, int quantity, boolean readyUp, double refund){
+        
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String totalMessage = "your total is $";
+        double newPrice = price - (refund * quantity);
 
-    double whiteMochaPrice = 5.5;
+        if(ready == true || readyUp == true){
+            if(refund > 0){
+            return (name + "! " + "Your new price is: $" + newPrice + ". Status: " + (ready? "Ready ":"Not Ready "));
+        }
+            ready = true;
+            return((ready? "Ready":"Not Ready") + ": " + name + readyMessage + " " + price);
+        }
+        else{
+            return("Not Ready: " + name + pendingMessage + ", and " + totalMessage + (price*quantity));
+        }
+    }
 
-    String customer1 = "Brendan Cordova";
+    public static void main(String[] args) {
+        
+        String greetingMessage = "Welcome to Cafe Java, ";
+        
+        double whiteMochaPrice = 5.5;
+        double espressoPrice = 4.0;
+        double dripPrice = 3.5;
+        double chaiPrice = 5.5;
 
-    boolean isreadyOrder1 = false;
+        String customer1 = "Brendan Cordova";
+        String customer2 = "Trae Hughes";
+        String customer3 = "Paul Soteropolis";
+        String customer4 = "Peter Marino";
 
-    out.println(greetingMessage + customer1)
+        boolean isreadyOrder1 = false;
+        boolean isreadyOrder2 = false;
+        boolean isreadyOrder3 = true;
+        boolean isreadyOrder4 = false;
 
+        out.println(greetingMessage + customer1);
+        out.println(orderUp(customer1, dripPrice, isreadyOrder1, 1, true, 0));
+        out.println(orderUp(customer2, espressoPrice, isreadyOrder2, 1, false, 0));
+        out.println(orderUp(customer3, whiteMochaPrice, isreadyOrder3, 2, false, 0));
+        out.println(orderUp(customer4, dripPrice, isreadyOrder4, 1, true, chaiPrice));
+    }
 }
